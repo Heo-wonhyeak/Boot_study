ce277b6b64fea3ba75983fb3ad8bea368d10adfb
안지호/플래닛기타 <anjo0080@gmail.com>
2022-09-21 19:22:25 +0900

    ./

--- hello-spring /src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring /src/main/java/hello/hellospring/controller/HelloController.java
@@ -2,14 +2,14 @@ package hello.hellospring.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@CrossOrigin(value = "*")
+@RequestMapping(value = "/api/test")
+@RestController
 public class HelloController {
 
-    @GetMapping("hello")
+    @GetMapping("/hello")
     public String hello(Model model) {
         model.addAttribute("data","hello!!");
         return "hello";
