2806ec0eccae5e90db1ba04d454d60c80bd39589
wonhyeakHeo <rkdcjf0147@naver.com>
2022-09-21 11:50:39 +0900

    frontcontrollerv4

---
+++ servlet/src/main/java/home/servlet/web/frontcontroller/v4/controller/MemberFormControllerV4.java
@@ -0,0 +1,12 @@
+package home.servlet.web.frontcontroller.v4.controller;
+
+import home.servlet.web.frontcontroller.v4.ControllerV4;
+
+import java.util.Map;
+
+public class MemberFormControllerV4 implements ControllerV4 {
+    @Override
+    public String process(Map<String, String> paramMap,Map<String,Object>model) {
+        return "new-form";
+    }
+}
