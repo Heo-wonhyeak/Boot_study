06dc7c033249efc44859b6ecca7db507a9e4f907
wonhyeakHeo <rkdcjf014777@gmail.com>
2022-10-22 10:06:26 +0900

    Show Download 경로 추가 (Down 경로에 저장된 이미지 파일 empty)

--- hello-spring /src/main/java/hello/hellospring/Utils/FileUploadUtil.java
+++ hello-spring /src/main/java/hello/hellospring/Utils/FileUploadUtil.java
@@ -13,11 +13,16 @@ import java.nio.file.StandardCopyOption;
 public class FileUploadUtil {
 
     public static String saveFile(String fileName, MultipartFile multipartFile) throws IOException {
-        Path uploadPath = Paths.get("Files-Upload");
+        Path uploadPathToShow = Paths.get("/Library/WebServer/Documents/front-demo/img/upload-files");
+        Path uploadPathToDownload = Paths.get("Files-Upload");
 
         // 업로드 패스에 파일이 없으면
-        if (!Files.exists(uploadPath)) {
-            Files.createDirectories(uploadPath);
+        if (!Files.exists(uploadPathToShow)) {
+            Files.createDirectories(uploadPathToShow);
+        }
+
+        if (!Files.exists(uploadPathToDownload)) {
+            Files.createDirectories(uploadPathToDownload);
         }
 
         // 파일코드 영문 숫자 조합 8자리
@@ -25,9 +30,11 @@ public class FileUploadUtil {
 
         try (InputStream inputStream = multipartFile.getInputStream()) {
             // 경로 조합하기 (고정 루트경로 + 부분 경로)
-            Path filePath = uploadPath.resolve(fileCode + "-" + fileName);
+            Path filePathToShow = uploadPathToShow.resolve(fileCode + "-" + fileName);
+            Path filePathToDownload = uploadPathToDownload.resolve(fileCode + "-" + fileName);
             // 복사할 경로 , 타겟 파일 경로 , 존재하더라도 덮어씀
-            Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
+            Files.copy(inputStream, filePathToShow, StandardCopyOption.REPLACE_EXISTING);
+            Files.copy(inputStream, filePathToDownload, StandardCopyOption.REPLACE_EXISTING);
         } catch (IOException e) {
             throw new IOException("파일이 저장되지 않았습니다" + fileName, e);
         }
