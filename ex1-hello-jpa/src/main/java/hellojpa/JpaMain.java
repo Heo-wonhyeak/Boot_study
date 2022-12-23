f414e8f71eaf62355a120b7502f2ac5d9793c28a
wonhyeakHeo <rkdcjf014777@gmail.com>
2022-12-23 22:21:47 +0900

    git add

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -21,29 +21,29 @@ public class JpaMain {
 
         //code
         try{
-//            //등록 C
-////            Member member = new Member();
-////
-////            member.setId(2L);
-////            member.setName("HelloJPA2");
+            //등록 C
+//            Member member = new Member();
 //
-//            //조회 R
-//            Member findMember = em.find(Member.class, 1L);
-//            System.out.println("findMember.getId() = " + findMember.getId());
-//            System.out.println("findMember.getName() = " + findMember.getName());
-//
-//            // 전체 조회
-//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-//                    .setFirstResult(1)
-//                    .setMaxResults(5)
-//                    .getResultList();
-//
-//            //수정 U
-//            findMember.setName("Hello~");
-//
-//            //삭제 D
-////            em.remove(findMember);
-////            em.persist(member);
+//            member.setId(2L);
+//            member.setName("HelloJPA2");
+
+            //조회 R
+            Member findMember = em.find(Member.class, 1L);
+            System.out.println("findMember.getId() = " + findMember.getId());
+            System.out.println("findMember.getName() = " + findMember.getName());
+
+            // 전체 조회
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(1)
+                    .setMaxResults(5)
+                    .getResultList();
+
+            //수정 U
+            findMember.setName("Hello~");
+
+            //삭제 D
+//            em.remove(findMember);
+//            em.persist(member);
 
             // 비영속
             Member member = new Member();
